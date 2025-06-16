b0e1296461046c2aaafeed51f18adfa6145da3a5
Ross1116 <roshan123243@gmail.com>
2025-06-16 12:12:23 +0530

    manifest creator

--- utils/manifest.go
+++ utils/manifest.go
@@ -1,5 +1,43 @@
 package utils
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type DefaultManifestManager struct{}
 
-// Will handle writing and reading manifest files (JSON metadata)
+type Manifest struct {
+	Filename string   `json:"filename"`
+	Chunks   []string `json:"chunks"`
+}
+
+func (c *DefaultManifestManager) SaveManifest(manifest Manifest, outputPath string) error {
+	content, err := json.MarshalIndent(manifest, "", " ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(outputPath, content, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *DefaultManifestManager) LoadManifest(inputPath string) (Manifest, error) {
+	var manifest Manifest
+
+	content, err := os.ReadFile(inputPath)
+	if err != nil {
+		return manifest, err
+	}
+
+	err = json.Unmarshal(content, &manifest)
+	if err != nil {
+		return manifest, err
+	}
+
+	return manifest, nil
+}
