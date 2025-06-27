f2cb0579c969b3665df218992effa7fd1631edf4
Ross1116 <roshan123243@gmail.com>
2025-06-27 14:59:56 +0530

    peer redistribute logic

--- handlers/chunk.go
+++ handlers/chunk.go
@@ -1,6 +1,11 @@
 package handlers
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 
@@ -18,4 +23,67 @@ func GetChunkHandler(c *gin.Context) {
 	}
 
 	c.File(path)
+}
+
+func RedistributeChunks(chunkHashes []string, peerURLS []string) error {
+	if len(peerURLS) == 0 {
+		return fmt.Errorf("no peers available")
+	}
+
+	for i, hash := range chunkHashes {
+		peerURL := peerURLS[i%len(peerURLS)]
+		chunkPath := utils.GetChunkPath(hash)
+
+		if err := UploadChunkToPeer(chunkPath, peerURL); err != nil {
+			log.Printf("Failed to upload the chunk %s to peer %s: %v", hash, peerURL, err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func UploadChunkToPeer(chunkPath string, peerURL string) error {
+	file, err := os.Open(chunkPath)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %v", err)
+	}
+	defer file.Close()
+
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+
+	part, err := writer.CreateFormFile("file", file.Name())
+	if err != nil {
+		return fmt.Errorf("failed to create form file: %v", err)
+	}
+
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return fmt.Errorf("failed to copy file data: %v", err)
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close writer: %v", err)
+	}
+
+	uploadEndpoint := peerURL + "/upload_chunk"
+	resp, err := http.Post(
+		uploadEndpoint,
+		writer.FormDataContentType(),
+		&requestBody,
+	)
+	if err != nil {
+		return fmt.Errorf("Unable to post the file to the peer %s: %v", peerURL, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("upload failed: %s\n%s", resp.Status, string(body))
+	}
+
+	fmt.Println("File uploaded successfully")
+	return nil
 }
--- peer/server/main.go
+++ peer/server/main.go
@@ -14,6 +14,7 @@ func ServeChunks(port string) {
 	router := gin.Default()
 	router.GET("/chunks/:hash", GetChunkHandler)
 	router.GET("/health", CheckHealthHandler)
+	// router.POST("/upload", UploadChunkHandler)
 
 	// port := choosePort("9000", "9001")
 	router.Run(":" + port)
@@ -43,3 +44,25 @@ func GetChunkHandler(c *gin.Context) {
 func CheckHealthHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "alive"})
 }
+
+// func UploadChunkHandler(c *gin.Context) {
+// 	file, err := c.FormFile("chunk")
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing File"})
+// 		return
+// 	}
+//
+// 	hash := c.PostForm("hash")
+// 	if hash == "" {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing hash"})
+// 		return
+// 	}
+//
+// 	savePath := filepath.Join("peer", "chunks", hash)
+// 	if err := c.SaveUploadedFile(file, savePath); err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save chunk"})
+// 		return
+// 	}
+//
+// 	c.JSON(http.StatusOK, gin.H{"message": "Chunk uploaded successfully"})
+// }
